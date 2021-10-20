f0922133cc9d996b7071906bd0272eed0a9d2e67
mgieseking <gieseking@informatik.uni-oldenburg.de>
2021-10-20 13:45:49 +0200

    Added a script to create Petri nets with transits from topology files.

---
+++ modelchecking/pnwtSDNCreation.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# @author Manuel Gieseking
+INPUTFOLDER=./sdn
+
+ADAM=../../deploy/adamMC
+
+MAX=$(ls -f $INPUTFOLDER/*.apt | wc -l)
+
+CUR_DATE=$(date +"%m%d%y")
+
+NAME=RU
+FOLDER=$CUR_DATE"_"$NAME
+mkdir -p $FOLDER
+echo "-------------------- Creating the Petri net with transits ------------------------------"
+count=1
+for filename in $INPUTFOLDER/*.apt; do
+	if [[ $(($count/2)) -lt $MAX ]]; then
+		FILE_NAME=$(basename "$filename" .apt)
+		FILE=$INPUTFOLDER/$FILE_NAME".apt"
+
+		$ADAM gen_topologie_zoo -i "$FILE" -c loopFreedom -o "$FOLDER"'/'"$FILE_NAME"
+	else
+		break;
+	fi
+done
